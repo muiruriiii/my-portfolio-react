f8afb4e52745cbad169f7aa15768eecb6ff398d3
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 10:19:41 +0300

    feat: add Contact component

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './contact.css';
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <div className="container">
+                <div className="row">
+                    <div className="contact-1">
+
+                        
+                    </div>
+                    <div className="contact-2">
+
+                    </div>
+                </div>
+            </div>
+            <p className="copyright">©2024 Cynthia Muiruri-Portfolio</p>
+        </section>
+    )
+}
+
+export default Contact;
