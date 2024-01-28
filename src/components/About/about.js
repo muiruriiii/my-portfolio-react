d5dc785f64e980afcf68c118381b4576e4491239
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 09:54:08 +0300

    feat(elements): add about column layout component

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -5,7 +5,14 @@ const About = () => {
     return (
         <section id="about">
             <div className="container">
+                <div className="row">
+                    <div className="about-col-1">
 
+                    </div>
+                    <div className="about-col-2">
+
+                    </div>
+                </div>
             </div>
         </section>
     );
