c0be7427ca287193a6cb7d6f4441b446b8aac07c
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 10:04:42 +0300

    feat: add Service Component

---
+++ src/components/Services/service.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './service.css';
+
+
+const Service = () => {
+    return (
+        <section id="services">
+            <div className="container">
+
+                <h1 className="subtitle2">WHAT I OFFER</h1>
+
+            </div>
+        </section>
+    );
+}
+
+export default Service;
