667229d8ee6e5a16f12a99ea4afad32c07a81248
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 08:34:04 +0300

    feat: add header section

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css';
+
+
+const Header = () => {
+    return (
+        <section id="header">
+            <div className="container">
+
+            </div>
+        </section>
+    );
+}
+
+export default Header;
