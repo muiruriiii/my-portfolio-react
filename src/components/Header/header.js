7a3acc14bd734a3bd303495c7a77e315ed091e93
muiruriiii <muiruricynthiaaa@gmail.com>
2024-01-28 09:06:40 +0300

    feat(elements): add navbar component

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,7 +6,16 @@ const Header = () => {
     return (
         <section id="header">
             <div className="container">
-
+                <nav>
+                    <h1><span className="logo">C</span>YNTHIA</h1>
+                    <ul>
+                        <li><a href="#header" >Home</a></li>
+                        <li><a href="#about" >About</a></li>
+                        <li><a href="#services" >Services</a></li>
+                        <li><a href="#projects" >Projects</a></li>
+                        <li><a href="#contact" >Contact</a></li>
+                    </ul>
+                </nav>
             </div>
         </section>
     );
